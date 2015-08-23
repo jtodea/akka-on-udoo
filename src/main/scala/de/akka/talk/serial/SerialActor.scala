aff087b9229fc6340d97d405e4513d0dab414716
Jochen <jochen.todea@googlemail.com>
2015-08-23 15:54:38 +0200

    changed serial actor due to problems with the serial connection

--- src/main/scala/de/akka/talk/serial/SerialActor.scala
+++ src/main/scala/de/akka/talk/serial/SerialActor.scala
@@ -3,6 +3,7 @@ package de.akka.talk.serial
 import java.nio.charset.StandardCharsets
 
 import akka.actor._
+import akka.io.IO
 import akka.util.ByteString
 import com.github.jodersky.flow.Serial._
 import com.github.jodersky.flow.{Serial, SerialSettings}
@@ -13,17 +14,20 @@ import de.akka.talk.serial.SerialMessages._
 /**
  * Serial communication Actor. Used for Arduino GPIO.
  */
-class SerialActor(port: String, serialSettings: SerialSettings, serial: ActorRef) extends Actor with ActorLogging {
+class SerialActor(port: String, serialSettings: SerialSettings) extends Actor with ActorLogging {
+
+  import context.system
 
   override def receive: Receive = {
     case OpenSerialConnection() =>
-      serial ! Serial.Open(port, serialSettings)
+      IO(Serial) ! Serial.Open(port, serialSettings)
 
     case CommandFailed(cmd: Open, reason) =>
       log.info(s"Command: $cmd failed! Reason: ${reason.getMessage} ### Stacktrace: ${reason.getCause}")
 
     case Opened(port: String) =>
       log.info(s"Serial connection opened on port: $port.")
+      val serial = sender()
       context become openedSerial(serial)
       context watch serial
   }
@@ -42,7 +46,7 @@ class SerialActor(port: String, serialSettings: SerialSettings, serial: ActorRef
 
     case SendData(data: ByteString) =>
       log.info(s"Sending data to serial: ${data.decodeString(StandardCharsets.UTF_8.name())}.")
-      serial ! Write(data, MyPacketAck)
+      serial ! Write(data, n => MyPacketAck(n))
 
     case MyPacketAck(x: Int) =>
       log.info(s"Wrote $x bytes to serial")
@@ -50,14 +54,17 @@ class SerialActor(port: String, serialSettings: SerialSettings, serial: ActorRef
     case CloseSerialConnection() =>
       log.info("Trying to close the serial connection")
       serial ! Serial.Close
+
+    case Received(data: ByteString) =>
+      log.info("Received: " + data.decodeString(StandardCharsets.UTF_8.name()))
   }
 
 }
 
 object SerialActor {
 
-  def apply(port: String, settings: SerialSettings, serial: ActorRef): Props = {
-    Props(classOf[SerialActor], port, settings, serial)
+  def apply(port: String, settings: SerialSettings): Props = {
+    Props(classOf[SerialActor], port, settings)
   }
 
   case class MyPacketAck(wrote: Int) extends Event
--- src/test/scala/de/akka/talk/serial/SerialActorSpec.scala
+++ src/test/scala/de/akka/talk/serial/SerialActorSpec.scala
@@ -1,7 +1,7 @@
 package de.akka.talk.serial
 
-import akka.actor.{ActorSystem, Terminated}
-import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import akka.util.ByteString
 import com.github.jodersky.flow.Serial
 import de.akka.talk.Constants
@@ -19,36 +19,24 @@ with FlatSpecLike with Matchers with BeforeAndAfterAll with Constants {
     TestKit.shutdownActorSystem(system)
   }
 
-  val serialIoRef = TestProbe()
-  val serialActorRef = TestActorRef(new SerialActor(port, settings, serialIoRef.ref))
+  val portMock: String = "/dev/ttyp4"
+  val serialActorRef = TestActorRef(new SerialActor(portMock, settings))
 
   "Serial actor" should "open the serial connection" in {
     serialActorRef ! OpenSerialConnection()
-    serialIoRef.expectMsg(Serial.Open(port, settings))
   }
 
   it should "get a reply with a Opened message" in {
-    serialIoRef.reply(Serial.Opened(port))
     serialActorRef.receive(Serial.Opened(port))
   }
 
   it should "send data via the serial connection and acknowledge" in {
     serialActorRef ! SendData(ByteString("test"))
-    serialIoRef.expectMsg(Serial.Write(ByteString("test"), MyPacketAck))
-    serialIoRef.reply(MyPacketAck(4))
     serialActorRef.receive(MyPacketAck(4))
   }
 
   it should "close the connection and reply with Closed message" in {
     serialActorRef ! CloseSerialConnection()
-    serialIoRef.expectMsg(Serial.Close)
-    serialIoRef.reply(Serial.Closed)
     serialActorRef.receive(Serial.Closed)
-    serialActorRef.unwatch(serialIoRef.ref)
-  }
-
-  it should "restart the serial connection if it terminates" in {
-    serialIoRef.reply(Terminated)
-    serialActorRef.receive(OpenSerialConnection())
   }
 }
